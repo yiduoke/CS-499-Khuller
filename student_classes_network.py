2af418fa471ce5be0cbf14dbf78264b6696d9615
yiduoke <yiduoke822@jhs185.org>
2021-10-22 07:11:06 -0500

    time conflicts now recognized

--- student_classes_network.py
+++ student_classes_network.py
@@ -11,7 +11,6 @@ credit_cap = 4
 
 student_to_courses_dict = {}
 course_to_students_dict = {}
-course_to_time_dict = {}
 
 def ParseCamelCase(string):
   return re.sub('([A-Z][a-z]*)', r' \1', re.sub('([A-Z]+)', r' \1', string)).split()
@@ -22,6 +21,7 @@ def parse_course_time(course_name):
   beginning_time_number = re.findall("[0-9]+:[0-9]+", course_name)
   beginning_and_end_times = re.findall("[0-9]+:[0-9]+ [A|P]M", course_name)
   
+  # converting Weekday hh:mm XM-hh:mm XM to integers
   for i in range(2):
     time = beginning_and_end_times[i]
     if time[-2:] == "AM" or (time[-2:] == "PM" and time[:2] == "12"):
@@ -32,7 +32,6 @@ def parse_course_time(course_name):
   time_index =  words_list.index(beginning_time_number[0])
   days_of_week = ParseCamelCase(words_list[time_index-1])
   time_ranges = [range(weekday_to_number_dict[x]*1440 + beginning_and_end_times[0], weekday_to_number_dict[x]*1440 + + beginning_and_end_times[1]) for x in days_of_week]
-  # union = set(time_ranges[0]).union(time_ranges[1])
   union = reduce((lambda x, y: x.union(y)), time_ranges, set([]))
   return union
 
@@ -43,8 +42,9 @@ with open('win22-requests-anon.csv', newline='') as csvfile:
     if (row[0] == "Name"): # skip header row
       continue
 
+    student = row[0]
+    student_to_courses_dict[student] = []
     for i in range(1,6):
-      student = row[0]
       course = row[i]
       if (course == "0"): # no class chosen
         continue
@@ -52,21 +52,24 @@ with open('win22-requests-anon.csv', newline='') as csvfile:
       x = model.addVar(vtype=GRB.BINARY, name=student + "_" + course)
       obj.addTerms(5-i, x) # the higher they rank the course, the higher the coefficient
 
-      if student in student_to_courses_dict:
-        student_to_courses_dict[student].append(course)
-      else:
-        student_to_courses_dict[student] = [course]
+      student_to_courses_dict[student].append(course)
       
       if course in course_to_students_dict:
         course_to_students_dict[course].append(student)
       else:
         course_to_students_dict[course] = [student]
-      
-      if course in course_to_time_dict:
-        course_to_time_dict[course].append(parse_course_time(course))
-      else:
-        print(course)
-        course_to_time_dict[course] = [parse_course_time(course)]
+    
+    model.update()
+    this_students_courses = student_to_courses_dict[student]
+    this_student_vars = [model.getVarByName(student + "_" + course) for course in this_students_courses]
+    cons = model.addConstr(sum(this_student_vars) <= 4) # student credit cap
+    # print(this_student_vars)
+    # print(cons)
+    for course in this_students_courses:
+      conflicts = filter(lambda x: len(parse_course_time(x).intersection(parse_course_time(course))) != 0, this_students_courses)
+      print("conflicts")
+      print(set(conflicts))
+
 
       
 
@@ -74,6 +77,6 @@ model.setObjective(obj, GRB.MAXIMIZE)
 
 
 
-print(parse_course_time("CS 336 Algorithms TuTh 09:30 AM-10:50 AM"))
+# print(parse_course_time("CS 336 Algorithms TuTh 09:30 AM-10:50 AM"))
 
 
