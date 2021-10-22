9c871c5a1c799fb96c804854e4dd39e12f322de2
yiduoke <yiduoke822@jhs185.org>
2021-10-22 05:44:57 -0500

    now need constraints

--- student_classes_network.py
+++ student_classes_network.py
@@ -1,4 +1,5 @@
 from gurobipy import *
+from functools import reduce
 import csv
 import re
 
@@ -12,10 +13,36 @@ student_to_courses_dict = {}
 course_to_students_dict = {}
 course_to_time_dict = {}
 
+def ParseCamelCase(string):
+  return re.sub('([A-Z][a-z]*)', r' \1', re.sub('([A-Z]+)', r' \1', string)).split()
+
+def parse_course_time(course_name):
+  weekday_to_number_dict = {"M": 0, "Tu": 1, "W": 2, "Th": 3, "F": 4} # don't have Saturday and Sunday
+  words_list = course_name.split(" ")
+  beginning_time_number = re.findall("[0-9]+:[0-9]+", course_name)
+  beginning_and_end_times = re.findall("[0-9]+:[0-9]+ [A|P]M", course_name)
+  
+  for i in range(2):
+    time = beginning_and_end_times[i]
+    if time[-2:] == "AM" or (time[-2:] == "PM" and time[:2] == "12"):
+      beginning_and_end_times[i] = int(time[:2]) * 60 + int(time[3:5])
+    else:
+      beginning_and_end_times[i] = 720 + int(time[:2]) * 60 + int(time[3:5])
+
+  time_index =  words_list.index(beginning_time_number[0])
+  days_of_week = ParseCamelCase(words_list[time_index-1])
+  time_ranges = [range(weekday_to_number_dict[x]*1440 + beginning_and_end_times[0], weekday_to_number_dict[x]*1440 + + beginning_and_end_times[1]) for x in days_of_week]
+  # union = set(time_ranges[0]).union(time_ranges[1])
+  union = reduce((lambda x, y: x.union(y)), time_ranges, set([]))
+  return union
+
 
 with open('win22-requests-anon.csv', newline='') as csvfile:
   student_courses_reader = csv.reader(csvfile, delimiter=',', quotechar='|')
   for row in student_courses_reader:
+    if (row[0] == "Name"): # skip header row
+      continue
+
     for i in range(1,6):
       student = row[0]
       course = row[i]
@@ -34,40 +61,19 @@ with open('win22-requests-anon.csv', newline='') as csvfile:
         course_to_students_dict[course].append(student)
       else:
         course_to_students_dict[course] = [student]
+      
+      if course in course_to_time_dict:
+        course_to_time_dict[course].append(parse_course_time(course))
+      else:
+        print(course)
+        course_to_time_dict[course] = [parse_course_time(course)]
 
       
 
 model.setObjective(obj, GRB.MAXIMIZE)
 
 
-def ParseCamelCase(string):
-  return re.sub('([A-Z][a-z]*)', r' \1', re.sub('([A-Z]+)', r' \1', string)).split()
 
-def parse_course_time(course_name):
-  weekday_to_number_dict = {"M": 0, "Tu": 1, "W": 2, "Th": 3, "F": 4} # don't have Saturday and Sunday
-  words_list = course_name.split(" ")
-  beginning_time_number = re.findall("[0-9]+:[0-9]+", course_name)
-  beginning_and_end_times = re.findall("[0-9]+:[0-9]+ [A|P]M", course_name)
-  
-  for i in range(2):
-    time = beginning_and_end_times[i]
-    if time[-2:] == "AM" or (time[-2:] == "PM" and time[:2] == "12"):
-      beginning_and_end_times[i] = int(time[:2]) * 60 + int(time[3:5])
-    else:
-      beginning_and_end_times[i] = 720 + int(time[:2]) * 60 + int(time[3:5])
-
-  print(beginning_and_end_times)
-  time_index =  words_list.index(beginning_time_number[0])
-  print("meow\n\n" + words_list[time_index-1])
-  days_of_week = ParseCamelCase(words_list[time_index-1])
-  time_ranges = [range(weekday_to_number_dict[x]*1440 + beginning_and_end_times[0], weekday_to_number_dict[x]*1440 + + beginning_and_end_times[1]) for x in days_of_week]
-  return time_ranges
-
-
-print(parse_course_time("CS 348 AI MWF 11:00 AM-11:50 AM"))
-print(ParseCamelCase("MWF"))
-print(ParseCamelCase("MW"))
-print(ParseCamelCase("M"))
-print(ParseCamelCase("TuTh"))
+print(parse_course_time("CS 336 Algorithms TuTh 09:30 AM-10:50 AM"))
 
 
