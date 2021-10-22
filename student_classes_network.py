858afcc0322e58a059fc069564c51332c6c23a95
yiduoke <yiduoke822@jhs185.org>
2021-10-22 03:58:52 -0500

    now need to encode class times and constraints

--- student_classes_network.py
+++ student_classes_network.py
@@ -3,35 +3,60 @@ import csv
 import re
 
 
-m = Model()
+model = Model()
+obj = LinExpr()
+
 credit_cap = 4
+
+student_to_courses_dict = {}
+course_to_students_dict = {}
+course_to_time_dict = {}
 
 def ParseCamelCase(string):
   return re.sub('([A-Z][a-z]*)', r' \1', re.sub('([A-Z]+)', r' \1', string)).split()
 
-with open('win22-requests-anon.csv', newline='') as csvfile:
-  student_classes_reader = csv.reader(csvfile, delimiter=',', quotechar='|')
-  for row in student_classes_reader:
-    print(', '.join([row[0],row[1],row[2],row[3],row[4],row[5]]))
 
-def parse_class_time(class_name):
-  words_list = class_name.split(" ")
-  class_time = re.findall("[0-9]+:[0-9]+", class_name)
-  print(class_time)
-  time_index =  words_list.index(class_time[0])
+
+with open('win22-requests-anon.csv', newline='') as csvfile:
+  student_courses_reader = csv.reader(csvfile, delimiter=',', quotechar='|')
+  for row in student_courses_reader:
+    for i in range(1,6):
+      student = row[0]
+      course = row[i]
+      if (course == "0"): # no class chosen
+        continue
+
+      x = model.addVar(vtype=GRB.BINARY, name=student + "_" + course)
+      obj.addTerms(5-i, x) # the higher they rank the course, the higher the coefficient
+
+      if student in student_to_courses_dict:
+        student_to_courses_dict[student].append(course)
+      else:
+        student_to_courses_dict[student] = [course]
+      
+      if course in course_to_students_dict:
+        course_to_students_dict[course].append(student)
+      else:
+        course_to_students_dict[course] = [student]
+
+      
+
+model.setObjective(obj, GRB.MAXIMIZE)
+
+def parse_course_time(course_name):
+  weekday_to_number_dict = {"M": 0, "Tu": 1, "W": 2, "Th": 3, "F": 4}
+  words_list = course_name.split(" ")
+  times = re.findall("[0-9]+:[0-9]+", course_name)
+  beginning_and_end_times = re.findall("[0-9]+:[0-9]+ [A|P]M", course_name)
+  print(beginning_and_end_times)
+  time_index =  words_list.index(times[0])
   return words_list[time_index-1]
 
 
-print(parse_class_time("CS 348 AI MWF 11:00 AM-11:50 AM"))
+print(ParseCamelCase(parse_course_time("CS 348 AI MWF 11:00 AM-11:50 AM")))
 print(ParseCamelCase("MWF"))
 print(ParseCamelCase("MW"))
 print(ParseCamelCase("M"))
 print(ParseCamelCase("TuTh"))
-
-
-
-
-
-# edge_s_name001 = m.addVar(lb=0, ub=credit_cap, obj = 0, vtype=GRB.INTEGER, name="edge_s_name001")
 
 
